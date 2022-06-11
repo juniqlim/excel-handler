a3b3c1ff2f47e15ee990ce2c13564a34ed71a9ea
juniq <juniqlim@gmail.com>
2022-06-11 20:24:48 +0900

    feat: 로드한 엑셀파일을 dataset으로 변환

--- src/main/java/Excel.java
+++ src/main/java/Excel.java
@@ -1,20 +1,41 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excel {
-    private final XSSFWorkbook workbook;
+    private final Workbook workbook;
 
     public Excel(File file) {
         this(workbook(file));
     }
 
-    public Excel(XSSFWorkbook workbook) {
+    public Excel(Workbook workbook) {
         this.workbook = workbook;
     }
 
-    private static XSSFWorkbook workbook(File file) {
+    public List<List<String>> dataset() {
+        Sheet sheet = workbook.getSheetAt(0);
+        List<List<String>> dataset = new ArrayList<>();
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+            Row row = sheet.getRow(i);
+            List<String> rows = new ArrayList<>();
+            for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
+                Cell cell = row.getCell(j);
+                rows.add(cell.toString());
+            }
+            dataset.add(rows);
+        }
+        return dataset;
+    }
+
+    private static Workbook workbook(File file) {
         try {
             return new XSSFWorkbook(file);
         } catch (IOException e) {
--- src/test/java/ExcelTest.java
+++ src/test/java/ExcelTest.java
@@ -1,7 +1,9 @@
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ExcelTest {
@@ -9,5 +11,14 @@ class ExcelTest {
     void 엑셀파일_로드() {
         File file = Paths.get("src/test/resources/sample.xlsx").toFile();
         assertThatCode(() -> new Excel(file)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 엑셀파일을_데이터셋으로_변환() {
+        File file = Paths.get("src/test/resources/sample.xlsx").toFile();
+        Excel excel = new Excel(file);
+        List<List<String>> dataset = excel.dataset();
+
+        assertThat(dataset.size()).isNotZero();
     }
 }
